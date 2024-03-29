173a49f5e0f6b3f7b5be05c17d33662cd7bfb71d
Gess1t <tintamare91@gmail.com>
2024-03-29 06:29:25 +0100

    Update README & ignore it

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Touch-Gestures User Documentation
 
 This is the repository for the user documentation of the Touch-Gestures project. 
+
 Most of the documentation is written in Markdown and is hosted on GitHub Pages.
--- conf.py
+++ conf.py
@@ -24,7 +24,7 @@ myst_enable_extensions = [
 ]
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'TODO.md']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'TODO.md', 'README.md']
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
